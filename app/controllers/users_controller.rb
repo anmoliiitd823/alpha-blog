a7f0a5185be19b43ff32c224b16aaf14274de5f7
anmoliiitd823 <anmol17220@iiitd.ac.in>
2017-12-12 01:27:09 +0530

    Add sign in when signing up:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			 session[:user_id] = @user.id
 			 flash[:success]="Welcome to Alpha Blog"
-			 redirect_to articles_path
+			 redirect_to user_path(@user)
 		else	
 			render 'new'
 		end
@@ -25,7 +26,6 @@ class UsersController < ApplicationController
 	end
 
 	def update
-		
 		if @user.update(user_params)
 			flash[:success]="Your account was successfully updated"
 			redirect_to articles_path
